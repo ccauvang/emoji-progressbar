1b2eb7f25b6dc22eb9b2fcd787fe024a9fe53f92
cauvang <90708399+ccauvang@users.noreply.github.com>
2022-11-17 00:56:03 +0700

    fix abc

--- progressBarEmoji.js
+++ progressBarEmoji.js
@@ -1,13 +1,12 @@
-function progressBarEmoji (value, maxValue) {
-	    let size = 5;
-        let barArray = [];
-        let fill = Math.round(size * (value / maxValue > 1 ? 1 : value / maxValue));
-        let empty = size - fill > 0 ? size - fill : 0;
-        for (let i = 1; i <= fill; i++) barArray.push("-fullbar-");
-        for (let i = 1; i <= empty; i++) barArray.push("-emptybar-");
-        barArray[0] = barArray[0] == "-fullbar-" ? "-fullstart-" : "-emptyStart-";
-        barArray[barArray.length - 1] = barArray[barArray.length - 1] == "-fullbar-" ? "-fillEnd-" : "-emptyEnd-";
+function progressBarEmoji (fullStart, fullBar, fullEnd, emptyStart, emptyBar, emptyEnd, value, maxValue, size) {
+	let barArray = [];
+        let full = Math.round(size * (value / maxValue > 1 ? 1 : value / maxValue));
+        let empty = size - full > 0 ? size - full : 0;
+        for (let i = 1; i <= full; i++) barArray.push(fullBar);
+        for (let i = 1; i <= empty; i++) barArray.push(emptyBar);
+        barArray[0] = barArray[0] == fullBar ? fullStart : emptyStart;
+        barArray[barArray.length - 1] = barArray[barArray.length - 1] == fullBar ? fullEnd : emptyEnd;
         return barArray.join(``);
     } 
     
-console.log(progressBarEmoji(90, 243))
+
